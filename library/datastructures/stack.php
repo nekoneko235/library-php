f7f810b031fde2a89958410469247c708111388b
Kenichi Yukihira <tyanyuki3.39@gmail.com>
2022-11-27 19:51:26 +0900

    Stackの例を追加

---
+++ library/datastructures/stack.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Problem Sbj: A51 - Stack
+Problem URL: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ay
+*/
+
+function stack($io = STDIN): void
+{
+    $q = trim(fgets($io));
+    $stack = [];
+    for ($i = 0; $i < $q; $i++) {
+        $arr = explode(" ", trim(fgets($io)));
+        switch ($arr[0]) {
+            case '2':
+                echo $stack[count($stack) - 1] . "\n";
+                break;
+            case '3':
+                array_pop($stack);
+                break;
+            default:
+                array_push($stack, $arr[1]);
+                break;
+        }
+    }
+}
+
+stack();
