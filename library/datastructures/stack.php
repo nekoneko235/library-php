774605d886dca9c32a8541cb2dd6b4baecfe7a8a
Kenichi Yukihira <tyanyuki3.39@gmail.com>
2022-11-27 21:20:01 +0900

    SplStackを使う

--- library/datastructures/stack.php
+++ library/datastructures/stack.php
@@ -8,18 +8,18 @@ Problem URL: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ay
 function stack($io = STDIN): void
 {
     $q = trim(fgets($io));
-    $stack = [];
+    $stack = new SplStack();
     for ($i = 0; $i < $q; $i++) {
         $arr = explode(" ", trim(fgets($io)));
         switch ($arr[0]) {
+            case '1':
+                $stack->push($arr[1]);
+                break;
             case '2':
-                echo end($stack) . "\n";
+                echo $stack->top() . "\n";
                 break;
             case '3':
-                array_pop($stack);
-                break;
-            default:
-                array_push($stack, $arr[1]);
+                $stack->pop();
                 break;
         }
     }
