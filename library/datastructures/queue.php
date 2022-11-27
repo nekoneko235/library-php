bca72328ab9433d42444fb23fb1e6a0c169857a0
Kenichi Yukihira <tyanyuki3.39@gmail.com>
2022-11-27 21:47:38 +0900

    SplQueueを使う

--- library/datastructures/queue.php
+++ library/datastructures/queue.php
@@ -8,18 +8,18 @@ Problem URL: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_az
 function queue($io = STDIN): void
 {
     $q = trim(fgets($io));
-    $queue = [];
+    $queue = new SplQueue();;
     for ($i = 0; $i < $q; $i++) {
         $arr = explode(" ", trim(fgets($io)));
         switch ($arr[0]) {
             case '1':
-                array_push($queue, $arr[1]);
+                $queue->push($arr[1]);
                 break;
             case '2':
-                echo $queue[0] . "\n";
+                echo $queue->bottom() . "\n";
                 break;
             case '3':
-                array_shift($queue);
+                $queue->shift();
                 break;
         }
     }
