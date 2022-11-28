393a22f9a9708f552499da5122a2ce213e3b033a
Kenichi Yukihira <tyanyuki3.39@gmail.com>
2022-11-28 23:38:13 +0900

    隣接リスト表現の例を追加

---
+++ library/graph/AdjacencyList.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+Problem Sbj: Adjacent Vertices
+Problem URL: https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bi
+*/
+
+function AdjacencyList($io = STDIN): void
+{
+    list($n, $m) = explode(" ", trim(fgets($io)));
+    $graph = [[]];
+    for ($i = 0; $i < $m; $i++) {
+        list($a, $b) = explode(" ", trim(fgets($io)));
+        $graph[$a][] = $b;
+        $graph[$b][] = $a;
+    }
+    for ($i = 1; $i <= $n; $i++) {
+        echo $i . ": {";
+        foreach ($graph[$i] as $key => $value) {
+            echo $value;
+            if ($key != count($graph[$i]) - 1) {
+                echo ", ";
+            }
+        }
+        echo "}\n";
+    }
+}
+
+AdjacencyList();
+
+/*
+[
+    '1' => ['1' => 2],
+    '2' => ['1' => 1, '2' => 3],
+    '3' => ['1' => 2, '2' => 4, '3' => 5],
+    '4' => ['1' => 3],
+    '5' => ['1' => 3],
+]
+*/
